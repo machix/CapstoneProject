3a4af99e85fbff172a2c9c95af668475dfe5aa54
NaturalFractals <jcochran800@gmail.com>
2018-02-02 15:39:12 -0500

    Added test for position endpoint.

--- backend/test/main_test.go
+++ backend/test/main_test.go
@@ -6,11 +6,12 @@ import (
 	"testing"
 )
 
+//Test basic get request to API
 func TestGetRequest(t *testing.T) {
 	response, err := http.Get("http://159.203.178.86:8000")
 
 	if err != nil {
-		t.Errorf("Not valid response from endpoint")
+		t.Errorf("Error. Not valid response from endpoint")
 	}
 
 	defer response.Body.Close()
@@ -22,6 +23,24 @@ func TestGetRequest(t *testing.T) {
 	}
 }
 
+//Test get requestion to /position endpoint
+func TestGetPositionRequest(t *testing.T) {
+	response, err := http.Get("http://159.203.178.86:8000/position")
+
+	if err != nil {
+		t.Errorf("Error. Not a valid response from endpoint.")
+	}
+
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	correctResponse := "{\"Text\":\"Soon you will get some really cool info herer! It will be very cool!\"}"
+
+	if !equal(correctResponse, body) {
+		t.Errorf("Incorrect response, %s", string(body[:len(body)]))
+	}
+}
+
+//Compares string and byte array to determine equality
 func equal(s string, b []byte) bool {
 	if len(s) != len(b) {
 		return false
