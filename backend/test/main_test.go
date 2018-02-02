4dc2733a31d2f30356af30cf8bdbcb784ce8ce72
NaturalFractals <jcochran800@gmail.com>
2018-02-02 15:24:58 -0500

    Added test for response of API on droplet.

--- backend/test/main_test.go
+++ backend/test/main_test.go
@@ -1,6 +1,35 @@
 package main
 
-import "testing"
+import (
+	"io/ioutil"
+	"net/http"
+	"testing"
+)
 
 func TestGetRequest(t *testing.T) {
+	response, err := http.Get("http://159.203.178.86:8000")
+
+	if err != nil {
+		t.Errorf("Not valid response from endpoint")
+	}
+
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	correctResponse := "Welcome, "
+
+	if !equal(correctResponse, body) {
+		t.Errorf("Incorrect response")
+	}
+}
+
+func equal(s string, b []byte) bool {
+	if len(s) != len(b) {
+		return false
+	}
+	for i, x := range b {
+		if x != s[i] {
+			return false
+		}
+	}
+	return true
 }
