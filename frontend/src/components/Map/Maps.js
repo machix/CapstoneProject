e2c58c7f32989c64bed416751d05e4a6e8a7c930
JesseCochran <jcochran800@gmail.com>
2018-02-23 19:11:26 -0500

    Added map with ability to draw polygons to frontend.

--- frontend/src/components/Map/Maps.js
+++ frontend/src/components/Map/Maps.js
@@ -1,22 +1,63 @@
 import React, { Component } from 'react';
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import { compose, withProps } from "recompose"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
 class Maps extends Component {
     render() {
+        const google = window.google;
+        const { compose, withProps } = require("recompose");
+        const {
+            withScriptjs,
+            withGoogleMap,
+            GoogleMap,
+        } = require("react-google-maps");
+        const { DrawingManager } = require("react-google-maps/lib/components/drawing/DrawingManager");
+
+        const MapWithADrawingManager = compose(
+            withProps({
+                googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places",
+                loadingElement: <div style={{ height: `100%` }} />,
+                containerElement: <div style={{ height: `500px`, width: '1400px' }} />,
+                mapElement: <div style={{ height: `100%` }} />,
+            }),
+            withScriptjs,
+            withGoogleMap
+        )(props =>
+            <GoogleMap
+                defaultZoom={8}
+                defaultCenter={new google.maps.LatLng(-34.397, 150.644)}
+                >
+                <DrawingManager
+                    defaultDrawingMode={google.maps.drawing.OverlayType.CIRCLE}
+                    defaultOptions={{
+                        drawingControl: true,
+                        drawingControlOptions: {
+                            position: google.maps.ControlPosition.TOP_CENTER,
+                            drawingModes: [
+                                google.maps.drawing.OverlayType.CIRCLE,
+                                google.maps.drawing.OverlayType.POLYGON,
+                                google.maps.drawing.OverlayType.POLYLINE,
+                                google.maps.drawing.OverlayType.RECTANGLE,
+                            ],
+                        },
+                        circleOptions: {
+                            fillColor: `#ffff00`,
+                            fillOpacity: 1,
+                            strokeWeight: 5,
+                            clickable: false,
+                            editable: true,
+                            zIndex: 1,
+                        },
+                    }}
+                    />
+            </GoogleMap>
+            );
         return (
-            <Map style={{ height:300 }}google={this.props.google} zoom={14}>
-                <Marker onClick={this.onMarkerClick}
-                    name={'Current location'} />
-                <InfoWindow onClose={this.onInfoWindowClose}>
-                    <div>
-                        <h1>Map</h1>
-                    </div>
-                </InfoWindow>
-            </Map>
+            <div>
+                <MapWithADrawingManager />
+            </div>
         );
     }
 }
 
-export default GoogleApiWrapper({
-    apiKey: ("AIzaSyDqJIIYPWM5hlaGmbENa9SaSSKWPtMOAKs")
-})(Maps)
+export default Maps;
