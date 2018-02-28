8ab1fe158612d0e0b8ae35ec95eba0072d83a7d5
NaturalFractals <jcochran800@gmail.com>
2018-02-28 16:38:56 -0500

    Refactored code for user and client. Added test for client.

---
+++ backend/database/database_client_test.go
@@ -0,0 +1,14 @@
+package database
+
+import (
+	"testing"
+)
+
+// Test connection to the AWS Postgres database
+func TestClientDatabaseConnect(t *testing.T) {
+	//Add Test to test database connection
+}
+
+// Test database save polygon for client database
+func TestDatabaseSavePolygon(t *testing.T) {
+}
--- backend/handlers/position_test.go
+++ backend/handlers/position_test.go
@@ -4,6 +4,12 @@ import (
 	"testing"
 )
 
+// Test GetPosition handler
 func TestGetPosition(t *testing.T) {
+
+}
+
+// Test PostPosition handler
+func TestPostPosition(t *testing.T) {
 
 }
