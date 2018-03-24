be3f23e2e5b241a9ba3f7da8b89ddec109bc337f
NaturalFractals <jcochran800@gmail.com>
2018-03-24 16:45:43 -0400

    Fixed conflict databae test.

--- backend/database/database_client_test.go
+++ backend/database/database_client_test.go
@@ -34,6 +34,30 @@ func TestClientDatabaseDeletePolygon(t *testing.T) {
 
 }
 
+// Test database fetching all current clients in database
+func TestClientDatabaseGetClient(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	rows := sqlmock.NewRows([]string{"id", "first_name", "last_name"}).AddRow(1, 4.5678, 5.4567)
+
+	mock.ExpectBegin()
+	mock.ExpectQuery(`[SELECT * FROM CLIENT]`).WillReturnRows(rows)
+
+	// Call GetClients method to return all clients from database
+	if err = GetClients(db); err != nil {
+		t.Errorf("error was not expected while updating stats: %s", err)
+	}
+
+	// Ensure the Expectations match
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
 // Test database adding a new client to database
 func TestClientDatabaseAddNewClient(t *testing.T) {
 
