f37a91ca9372d324cb907088a47a5f24256a920d
NaturalFractals <jcochran800@gmail.com>
2018-03-28 17:31:52 -0400

    Refactored out polygon code from client.

--- backend/database/database_polygon.go
+++ backend/database/database_polygon.go
@@ -1,1 +1,88 @@
 package database
+
+import (
+	"database/sql"
+	"fmt"
+	"strconv"
+
+	"github.com/NaturalFractals/CapstoneProject/backend/model"
+)
+
+// Returns all polygons associated with client
+func GetPolygons(c *model.Client, db *sql.DB) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	sqlStmt := "SELECT polygon FROM CLIENT_POLYGON WHERE id=$1"
+
+	clientPolygonRetrieve, err := tx.Prepare(sqlStmt)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	defer clientPolygonRetrieve.Close()
+
+	_, err = tx.Exec(sqlStmt, c.ID)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return nil
+}
+
+// Saves points in a polygon that has been drawn on the map
+func SavePolygon(p *model.Polygon, c *model.Client, db *sql.DB) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	sqlStmt := "INSERT INTO CLIENT_POLYGON (id, name, polygon) VALUES ($1, $2, $3)"
+
+	// Format the Polygon string for insertion into the database
+	polygonStmt := "POLYGON(("
+	for _, h := range p.Coordinates {
+		for _, i := range h {
+			polygonStmt += FloatToString(i) + " "
+		}
+		polygonStmt += ","
+	}
+	polygonStmt += "))"
+
+	fmt.Println(polygonStmt)
+
+	_, err = tx.Exec(sqlStmt, p.Id, p.Name, polygonStmt)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return nil
+}
+
+// Delete a polygon that has been saved in the database
+func DeletePolygon(p *model.Polygon, c *model.Client, db *sql.DB) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	sqlStmt := "DELETE * WHERE id=$1 and polygon=$2"
+
+	_, err = tx.Exec(sqlStmt, c.ID, p)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return nil
+}
+
+// Convert Float number to a string
+func FloatToString(input_num float64) string {
+	return strconv.FormatFloat(input_num, 'f', 6, 64)
+}
--- backend/database/database_polygon_test.go
+++ backend/database/database_polygon_test.go
@@ -1,1 +1,18 @@
 package database
+
+import "testing"
+
+// Test retrieving polgons from the database
+func TestGetPolygons(t *testing.T) {
+
+}
+
+// Test saving polygons to the database
+func TestSavePolygon(t *testing.T) {
+
+}
+
+// Test deleting polygon from the database
+func TestDeletePolygon(t *testing.T) {
+
+}
