e54e97365c13c5798a1d0dc50a5e4492544275b6
NaturalFractals <jcochran800@gmail.com>
2018-04-03 22:03:26 -0400

    Boilerplate functions for testing geo utils.

--- backend/geofence/geo_utils_test.go
+++ backend/geofence/geo_utils_test.go
@@ -2,5 +2,27 @@ package geo
 
 import "testing"
 
+// Test the utility to see if two polygons have intersecting edges
+// This will inherently check the checkInsersect method
 func TestHaveIntersectingEdges(t *testing.T) {
+}
+
+// Test the project function to determine projection
+func TestProject(t *testing.T) {
+
+}
+
+// Test the hasPointInPolygon function
+func TestHasPointInPolygon(t *testing.T) {
+
+}
+
+// Test vectorDifference
+func TestVectorDifference(t *testing.T) {
+
+}
+
+// Test vectorCrossProdcut
+func TestVectorCrossProduct(t *testing.T) {
+
 }
