f2015afca148cd477597fc61e54739d5bcb00f5d
NaturalFractals <jcochran800@gmail.com>
2018-04-03 22:12:26 -0400

    Added test for geofence utils.

--- backend/geofence/geo_utils_test.go
+++ backend/geofence/geo_utils_test.go
@@ -1,6 +1,11 @@
 package geo
 
-import "testing"
+import (
+	"testing"
+
+	geo "github.com/kellydunn/golang-geo"
+	"github.com/stretchr/testify/assert"
+)
 
 // Test the utility to see if two polygons have intersecting edges
 // This will inherently check the checkInsersect method
@@ -19,10 +24,22 @@ func TestHasPointInPolygon(t *testing.T) {
 
 // Test vectorDifference
 func TestVectorDifference(t *testing.T) {
+	point1 := geo.NewPoint(102.3231, 104.2243)
+	point2 := geo.NewPoint(108.2352, 107.4234)
 
+	test1 := vectorDifference(point1, point2)
+
+	pointTest1 := geo.NewPoint(-5.9121000000000095, -3.1991000000000014)
+
+	assert.Equal(t, test1, pointTest1, "They should be equal.")
 }
 
 // Test vectorCrossProdcut
 func TestVectorCrossProduct(t *testing.T) {
+	point1 := geo.NewPoint(102.3231, 104.2243)
+	point2 := geo.NewPoint(108.2352, 107.4234)
 
+	test1 := vectorCrossProduct(point1, point2)
+
+	assert.Equal(t, test1, -288.84265482000046, "They should be equal.")
 }
