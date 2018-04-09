b160eeb8b8c228eacedeee21dc300c52fef2644d
NaturalFractals <jcochran800@gmail.com>
2018-04-09 00:52:34 -0400

    Fixed testing of mock database with new interface.

--- backend/database/database_user_test.go
+++ backend/database/database_user_test.go
@@ -12,6 +12,10 @@ func TestUserDatabaseConnect(t *testing.T) {
 	// Add Test to test database connection
 }
 
+type Env struct {
+	db model.Datastore
+}
+
 // Test get request to database for user
 func TestDatabaseSelectQuery(t *testing.T) {
 	db, mock, err := sqlmock.New()
@@ -19,6 +23,7 @@ func TestDatabaseSelectQuery(t *testing.T) {
 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 	}
 
+	env := &DB{db}
 	defer db.Close()
 
 	rows := sqlmock.NewRows([]string{"Id", "Latitude", "Longitude"}).AddRow(1, 4.5678, 5.4567)
@@ -29,7 +34,7 @@ func TestDatabaseSelectQuery(t *testing.T) {
 	tempSummary := model.Summary{}
 
 	// Test QueryPosition function
-	if err = QueryPosition(&tempSummary, db); err != nil {
+	if err = env.QueryPosition(&tempSummary); err != nil {
 		t.Errorf("error was not expected while updating stats: %s", err)
 	}
 
@@ -45,11 +50,12 @@ func TestDatabaseInsertQuery(t *testing.T) {
 }
 
 // Test database delete query for user
-func TestDatabaseDeleteQuery(t *testing.T) {
+func (env *Env) TestDatabaseDeleteQuery(t *testing.T) {
 	db, mock, err := sqlmock.New()
 	if err != nil {
 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 	}
+
 	defer db.Close()
 
 	sqlmock.NewRows([]string{"Id", "Latitude", "Longitude"}).AddRow(1, 4.5678, 5.4567)
