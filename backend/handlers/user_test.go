1df223a65c38c10dbaa064050f0cf3391374f2b6
NaturalFractals <jcochran800@gmail.com>
2018-04-09 22:55:54 -0400

    Finished test for user handlers.

--- backend/handlers/user_test.go
+++ backend/handlers/user_test.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"bytes"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -10,6 +11,7 @@ import (
 
 type mockDB struct{}
 
+// Interface implementation for mockDB
 func (mdb *mockDB) QueryPosition(us *model.Summary) error {
 	us.UserSummary = append(us.UserSummary, model.User{Id: 1, Latitude: 1.23, Longitude: 1.23})
 	return nil
@@ -29,18 +31,44 @@ func TestGetPosition(t *testing.T) {
 	}
 }
 
-func (mdb *mockDB) DeletePosition(u *model.User) error {
-	return nil
-}
-
-//Test delete user position
-func TestDeletePosition(t *testing.T) {
-}
-
+// Interface implementation for mockDB
 func (mdb *mockDB) PostPosition(u *model.User) error {
+	u = &model.User{Id: 1, Latitude: 1.34, Longitude: 1.34}
 	return nil
 }
 
 // Test post user position
 func TestPostPosition(t *testing.T) {
+	var b bytes.Buffer
+	rec := httptest.NewRecorder()
+	req, _ := http.NewRequest("DELETE", "/postPosition", &b)
+
+	env := Env{db: &mockDB{}}
+	http.HandlerFunc(env.PostPosition).ServeHTTP(rec, req)
+
+	expected := "{\"Id\":0,\"Latitude\":0,\"Longitude\":0}"
+	if expected != rec.Body.String() {
+		t.Errorf("\n...expected = %v\n...obtained = %v", expected, rec.Body.String())
+	}
+}
+
+// Interface implementation for mockDB
+func (mdb *mockDB) DeletePosition(u *model.User) error {
+	u = &model.User{Id: 1, Latitude: 1.45, Longitude: 1.45}
+	return nil
+}
+
+//Test delete user position
+func TestDeletePosition(t *testing.T) {
+	var b bytes.Buffer
+	rec := httptest.NewRecorder()
+	req, _ := http.NewRequest("DELETE", "/deletePosition", &b)
+
+	env := Env{db: &mockDB{}}
+	http.HandlerFunc(env.DeletePosition).ServeHTTP(rec, req)
+
+	expected := "{\"Id\":0,\"Latitude\":0,\"Longitude\":0}"
+	if expected != rec.Body.String() {
+		t.Errorf("\n...expected = %v\n...obtained = %v", expected, rec.Body.String())
+	}
 }
