3ec99660e107d051b82029c360cbacd003a1e7a5
NaturalFractals <jcochran800@gmail.com>
2018-04-11 23:22:03 -0400

    Add test for geo package. Add start of quad tree.

--- backend/geofence/quadtree.go
+++ backend/geofence/quadtree.go
@@ -1,1 +1,20 @@
 package geo
+
+// QuadTree data structure
+type QuadTree struct {
+	Bounds    Bounds
+	MaxItem   int
+	MaxHeight int
+	Height    int
+	Item      []Bounds
+	Nodes     []QuadTree
+	Total     int
+}
+
+// Bounding box with an x,y origin and width, heigh
+type Bounds struct {
+	X      float64
+	Y      float64
+	Width  float64
+	Height float64
+}
--- backend/handlers/geofence_test.go
+++ backend/handlers/geofence_test.go
@@ -1,13 +1,35 @@
 package handlers
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+// Test create geofence endpoint
+func TestCreateGeofence(t *testing.T) {
+	rec := httptest.NewRecorder()
+	json := `{"Id": "1", "Name": "polygon1", "Coordinates": "[ {Latitude: 1.23, Longitude: 1.23}, {Latitude: 2.34, Longitude: 2.34},
+		{Latitude: 4.56, Longitude: 4.56}, {Latitude: 1.23, Longitude: 1.23}]" }`
+	req, _ := http.NewRequest("POST", "/createGeofence", strings.NewReader(json))
+	req.Header.Set("Content-Type", "application/json")
+
+	http.HandlerFunc(CreateGeofence).ServeHTTP(rec, req)
+
+	// Check the status code is what we expect.
+	if status := rec.Code; status != http.StatusInternalServerError {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusInternalServerError)
+	}
+}
 
 // Test point in polygon endpoint
 func TestCheckPointInPolygon(t *testing.T) {
 
 }
 
-// Test polygon overlap endpoint
+// Test polygon overlap handler
 func TestPolygonOverlap(t *testing.T) {
 
 }
